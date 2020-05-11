0e1c6716506aeacfcbb870ef290341146333eb2a
if19b050 <if19b050@technikum-wien.at>
2020-05-11 11:36:38 +0200

    Update start.js

--- node_templates/starting-page/start.js
+++ node_templates/starting-page/start.js
@@ -49,32 +49,3 @@ postLoad.style.opacity = 1;
 function start(){
 	location.replace(location.origin + "/websites/01_robox.html")
 }
-
-const websocketReloading = new WebSocket('ws://' + location.hostname + ':' + location.port + '/reload');
-var load = false;
-websocketReloading.addEventListener("message", (ws) =>{
-    if (load == false) {
-        load = true
-        location.replace(location.origin + "/" + ws.data)
-    }
-})
-
-const websocketMediaControl = new WebSocket('ws://' + location.hostname + ':' + location.port + '/media');
-var media = false;
-websocketMediaControl.addEventListener("message", (ws) =>{
-    if (media == false) {
-        media = true
-        console.log(ws)
-    }
-})
-websocketMediaControl.addEventListener("open", (ws) =>{
-        console.log(ws)
-        websocketMediaControl.send("Media Control is ready!")
-
-        var video = document.getElementsByTagName("video")
-        if(video.length > 0){
-            websocketMediaControl.send("Side has a video!")
-        }else{
-            websocketMediaControl.send("Side has no video!")
-        }
-})
